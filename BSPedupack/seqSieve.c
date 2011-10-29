22ae9ddf263a7492639a57b8282518f71938577a
Davide Taviani <helios89@gmail.com>
2011-10-29 13:59:55 +0200

    fix

--- BSPedupack/seqSieve.c
+++ BSPedupack/seqSieve.c
@@ -1,32 +1,33 @@
 #include <math.h>
+#define ull unsigned long long
 
-int *primes;
+ull *primes;
 int count = 0;
 
-int seqSieve(int N){
-
-
-  int arr[N];
-
-  int i=0;
-  int k=2;
-
+ull seqSieve(ull N){
+  
+  
+  ull arr[N];    
+  
+  ull i=0;
+  ull k=2;
+  
   // Populates the array with only the even numbers
-
+  
   while(k<=N){
     arr[i] = k;
     // printf("arr[%d] = %d\n",i,k);
     i++;
     k = 2*i+1;
   }
+  
+  ull len = i;
 
-  int len = i;
-
-  int j;
-  int bound = ((int)floor(sqrt(2*len+1)))/2;
-  int m;
-  // double h = sqrt(N);
-
+  ull j;
+  ull bound = ((int)floor(sqrt(2*len+1)))/2;
+  ull m;
+  //  double h = sqrt(N);  
+  
   for(i=1;i<=bound;i++)
   {
     m = arr[i];
@@ -35,33 +36,27 @@ int seqSieve(int N){
       arr[j] = 0;
     }
     }
+    
+    //  printf("bound is %d\n",bound);
+    //  printf("len is %d\n",len);
+  
 
-    // printf("bound is %d\n",bound);
-    // printf("len is %d\n",len);
-
-
-
-
- primes = (int *) malloc (count * sizeof(int));
- j=0;
-
-     for(i=0;i<=len;i++){
+    for(i=0;i<=len;i++){
       if (arr[i]!=0) {
         count++;
-		primes[j] = arr[i];
       }
     }
-
+  
   count--;
-  //primes = (int *) malloc (count * sizeof(int));
-
-  //  j=0;
-
-  //  for(i=0;i<len;i++){
-  //    if(arr[i]!=0){
-  //      primes[j] = arr[i];
-  //      j++;
-  //    }
-  //  }
-
+  primes = (ull *) malloc (count * sizeof(unsigned lon));
+  
+    j=0;
+  
+    for(i=0;i<len;i++){
+      if(arr[i]!=0){
+        primes[j] = arr[i];
+        j++;
+      }
+    }    
+    
   }
