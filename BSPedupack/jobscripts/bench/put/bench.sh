8c431f2200fbb6b23ea51654b6893e578bd1b2f8
Davide Taviani <helios89@gmail.com>
2011-11-06 12:14:40 +0100

    typo

--- BSPedupack/jobscripts/bench/put/bench.sh
+++ BSPedupack/jobscripts/bench/put/bench.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-cd /home/bissstud/~/Students11/d.taviani/parAlg/BSPedupack
+cd /home/bissstud/Students11/d.taviani/parAlg/BSPedupack
 #mpcc -o bench bspbench.c bspedupack.c -lbsponmpi
 ./bench $1 
